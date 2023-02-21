54851faa3516aa1aafbc9eb513fdc07e5188600a
piedracita <arnaldpedros@gmail.com>
2023-02-21 09:57:10 +0100

    Create bodas.php

---
+++ bodas.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- COMMON-HEAD -->
+    <?php include "common-php/head.html"; ?>
+    <!-- COMMON-HEAD END -->
+
+    <!-- CANONICAL -->
+    <link rel="canonical" href="https://www.saxonara.com/saxofonista-para-bodas.php">
+
+    <!-- COMMON-LOCALIZATION -->
+    <?php include "common-php/localization/contact.html"; ?> <!-- edit -->
+    <!-- COMMON-LOCALIZATION END -->
+
+    <!-- SITE INFORMATION -->
+    <title>Saxofonista para bodas | SAXONARA</title>
+    <meta name="description" content="Toda la información sobre como contratar un saxofonista para tu boda de la mano de SAXONARA, entretenimiento con saxofonista profesional para bodas">
+    <meta name="keywords" content="saxofonista, bodas, entretenimiento, barra libre, cóctel, cocktail, ceremonia">
+
+    <!-- COMMON-OPENGRAPH -->
+    <?php include "common-php/opengraph/es.html"; ?>
+    <!-- COMMON-OPENGRAPH END -->
+
+</head>
+
+<body class="page">
+
+    <!-- COMMON-BODY -->
+    <?php include "common-php/body.html"; ?>
+    <!-- COMMON-BODY END -->
+
+    <header id="top" class="top-header">
+        <!-- NAVIGATION -->
+        <?php include "common-php/navigation/es.html"; ?>
+        <!-- NAVIGATION END -->
+    </header>
+
+    <!-- PAGE HEADER -->
+    <div class="page-header">
+        <div class="container text-center">
+            <h1 class="page-title">Saxofonista para Bodas</h1>
+            <ol class="breadcrumb">
+                <li>Todo sobre como mezclar música y entretenimiento para tu boda</li>
+            </ol>
+        </div>
+    </div>
+    <!-- PAGE HEADER END -->
+
+    <div class="main-wrap">
+
+        <!-- COMMON WEDDINGS -->
+        <?php include "common-php/weddings/es.html"; ?>
+        <!-- COMMON WEDDINGS END -->
+
+        <div class="section section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 col-xs-12">
+                        <div class="text-center">
+                            <h3 class="section-title" id="wedding-ceremony">La Ceremonia</h3>
+                            <p>La primera intervención en la boda como músico es en la parte de la ceremonia, con el camino al altar de los novios. ¿Te gustaría una canción <strong>moderna</strong> o un <strong>clásico</strong> de las bodas? En parejas que prefieren lo moderno, la música de <strong>Ed Sheeran</strong> es una de las más solicitadas, mientras que en ceremonias tradicionales un <strong>Canon de Pachelbel</strong> o la <strong>Marcha Nupcial de Wagner</strong> son los referentes.</p>
+
+                            <p>Consulta todo mi repetorio en la <a href="/repertoire.php">pestaña REPERTORIO</a>, toma ideas de que canciones te gustarían para cada momento de la Ceremonia, si no encuentras tu canción favorita, avísame, no será ningún problema aprenderla para tu boda.</p>
+
+                            <p>También puedes encontrar muchos de mis vídeos buscando <a href="#contact">SAXONARA en YouTube o Instagram</a>, escucha mis versiones en saxofón y imagínate como sonaría el día de tu boda.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- VIDEOS CEREMONY -->
+                <?php include "common-php/videos/wedding-ceremony.html"; ?>
+                <!-- VIDEOS CEREMONY END -->
+            </div>
+        </div>
+
+        <!-- ABOUT SINGER -->
+        <div class="section about-singer-section text-white">
+            <div class="section-padding">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-7">
+                            <div class="about-singer-content">
+                                <h3 class="about-singer-title">Amplio repertorio</h3>
+                                <p class="about-singer-role">& Presentación elegante</p>
+                                <p class="about-singer-text">La experienca me permite <b>adaptarme a distintos eventos y fiestas</b>, tocando estilos como <b>Tropical House, Smooth Pop</b> y improvisando junto a <strong>DJs con luces LED en el saxofón</strong>.</p>
+                                <p class="about-singer-text">Dispongo de un <b>amplio repertorio</b> que os ayudará a tomar ideas y estoy <b>abierto a peticiones</b>, especialmente en bodas.</p>
+                                <p class="about-singer-text">El <strong>cuidadoso código de vestimenta</strong> es tan importante como la música, así me presentaré vestido de manera <b>elegante</b> o acorde a la situación, ¡envíame tus comentarios!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ABOUT SINGER END -->
+
+    	<div class="section section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 col-xs-12">
+                        <div class="text-center">
+                            <h3 class="section-title" id="wedding-cocktail">Cóctel de Bienvenida & Aperitivo</h3>
+                            <p>Mezcla de música y entretenimiento, el Cóctel o Aperitivo es un momento excelente para disfrutar de <strong>música en directo con saxofón</strong>, con un repertorio Tropical House al más puro estilo Ibiza y amenizando en todos los espacios aprovechando que dispongo de microfonía inalámrbica.</p>
+
+                            <p>Además prepararemos, si los novios así lo quieren, una <strong>entrada espectacular al Aperitivo</strong> con música bien explosiva para que sea increíble, paseando entre los invitados y bailando con aquellos que más se presten.</p>
+
+                            <p>¡No os olvidéis de decirme cuales son vuestras canciones favoritas para así añadirlas a mi repertorio!</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- VIDEOS COCKTAIL -->
+                <?php include "common-php/videos/wedding-cocktail.html"; ?>
+                <!-- VIDEOS COCKTAIL END -->
+            </div>
+        </div>
+
+        <div class="section section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 col-xs-12">
+                        <div class="text-center">
+                            <h3 class="section-title" id="wedding-party">La Fiesta & Barra Libre</h3>
+                            <p>Una salida espectacular tras el Primer Baile o al Final del Banquete, con luces LED en el saxofón y un <b>estilo musical explosivo</b>, así me gusta arrancar la <strong>fiesta junto al DJ</strong> ¡por sorpresa y con energía!</p>
+
+                            <p>Mi versatilidad al saxofón me permite adaptarme a cada tipo de evento, así la Barra Libre es una excelente opción par sacar <b>el sonido más funky al saxofón</b> y animar en la pista de baile con la música más moderna y también esos clásicos de la música Disco.</p>
+
+                            <p>Para estos momentos me encanta usar mi <strong>saxofón con luces LED</strong>, ofreciendo un show más vistoso y espectacular, que haga relucir al máximo este momento final de la celebración en tu boda.</p>
+
+                            <p>Trabajo con distintos DJs y también me adapto al que tengáis contratado, estando en constante comunicación, facilitándole mi música para que pueda mezclarla y, incluso, <b>improvisando junto a sus mezclas en la pista de baile</b>. ¡Que empiece la fiesta!</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- VIDEOS DJSAX -->
+                <?php include "common-php/videos/djsax.html"; ?>
+                <!-- VIDEOS DJSAX END -->
+            </div>
+        </div>
+
+        <!-- REVIEWS -->
+        <?php include "common-php/reviews/es.html"; ?>
+        <!-- REVIEWS END -->
+
+    </div>
+
+    <!-- FOOTER -->
+    <?php include "common-php/footer/es.html"; ?>
+    <!-- FOOTER END -->
+
+    <!-- COMMON-BODY-SCRIPT -->
+    <?php include "common-php/body-script.html"; ?>
+    <!-- COMMON-BODY-SCRIPT END -->
+
+</body>
+
+</html>

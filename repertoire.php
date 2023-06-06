76f88d7b7a107eae69f082a042512ddafc7941dc
piedracita <arnaldpedros@gmail.com>
2023-06-06 11:18:56 +0200

    update spanish songlist

--- repertoire.php
+++ repertoire.php
@@ -36,7 +36,7 @@
     <div class="page-header">
         <div class="container text-center">
             <h1 class="page-title">Repertorio y Estilos</h1>
-            <h5>Gran selección musical y variedad de estilos por SAXONARA</h5>
+            <h5>Resumen de los distintos estilos de música que interpreto en saxofón, según sea para un evento lounge, cocktail, fiesta o incluso ceremonia de boda</h5>
             <!-- <ol class="breadcrumb">
                 <li>Selección musical con grabaciones de estudio y vídeos en directo</li>
             </ol> -->
@@ -51,17 +51,17 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 col-xs-12">
                         <div class="text-center">
-                            <h3 class="section-title">SELECCIÓN MUSICAL</h3>
-                            <p class="section-subtext">A lo largo de los años como saxofonista profesional, trabajando junto a bandas y DJs, así como en distintos tipos de eventos, he creado una <b>selección musical</b> de aquellas piezas que más resaltan la autenticidad y sonido del saxofón. Algunas de ellas las he agrupado a continuación, para que puedan escoger aquellas <strong>canciones que no pueden faltar en el día de su boda o evento</strong> especial.</p>
+                            <h3 class="section-title" style="text-transform: uppercase;">La mejor música para la fiesta</h3>
+                            <p class="section-subtext">A continuación encontraras un seguido de playlists y vídeos de distintos estilos, todos ellos para ser <strong>interpretados en saxofón</strong> y es que, tras años de experiencia como <strong>saxofonista profesional en eventos y bodas</strong>, he desarrollado un repertorio súper extenso que se adapta a muchas necesidades y, sino, lo adapto y incluyo nuevos temas en el repertorio.</p>
 
-                           	<p>Recuerden que adapto el repertorio al gusto del cliente y estoy abierto a <strong>peticiones musicales</strong> que puedan tener, si no encuentran su pieza favorita en mi repertorio, avísenme, no hay nada más emocionante que aprender nuevas canciones.</p>
+                           	<p>Esta primera selección son canciones en formato <strong>fiesta DJ y saxofonista</strong>, en <strong>discotecas o fiestas de boda</strong>, lo que se conoce como <strong>Barra Libre</strong>. Música comercial que pone a todo el mundo a bailar, interpretada en saxofón desde la pista de baile con la opción de incluir <strong>luces LED en el saxofón</strong>. También son opciones perfectas para la <strong>entrada al banquete de boda</strong> o al aperitivo.</p>
                         </div>
                     </div>
                 </div>
 
-                <!-- COMMON SONG-LIST -->
-                <?php include "common-php/song-list.html"; ?>
-               	<!-- COMMON SONG-LIST END -->
+                <!-- COMMON -->
+                <?php include "common-php/song-list/uplifting.html"; ?>
+                <!-- COMMON -->
                 
             </div>
         </div>
@@ -72,7 +72,7 @@
                     <div class="row">
                         <div class="col-md-8 col-md-offset-2 col-xs-12">
                             <div class="text-center">
-                                <h3 class="section-title">BALADAS Y MÚSICA ROMÁNTICA</h3>
+                                <h3 class="section-title" style="text-transform: uppercase;">Baladas y música romántica para ceremonia</h3>
                                 <p class="section-subtext">Piezas románticas, modernas y tranquilas, fruto de la variedad musical que he interpretado a lo largo de eventos así como <strong>peticiones de clientes</strong> que añadido al repertorio, especialmente de parejas en bodas, y más especialmente aún en <strong>ceremonias de boda</strong>, en las que he participado como saxofonista; pero también en momentos como ambientación en cenas u otro tipo de eventos tranquilos.</p>
 								<p>Un estilo que me encanta para poder <b>sacar el máximo partido de la expresividad de mis saxofones</b>, utilizando el más idóneo en cada momento y eligiendo entre el saxofón tenor, saxofón alto y saxofón soprano.</p>
                             </div>
@@ -80,9 +80,9 @@
                     </div>
 
                     <!-- COMMON -->
-                    <?php include "common-php/videos/wedding-ceremony.html"; ?>
+                    <?php include "common-php/song-list/ballads.html"; ?>
                     <!-- COMMON -->
-                    
+
                 </div>
             </div>
         </div>
@@ -92,19 +92,16 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 col-xs-12">
                         <div class="text-center">
-                            <h3 class="section-title">FIESTA & BARRA LIBRE</h3>
-                            <p class="section-subtext">Piezas explosivas en estilo <strong>Disco, Reggaeton y Electrónico</strong> que interpreto en saxofón junto con DJs locales en clubs o fiestas tipo <strong>Barra libre de Boda</strong>, muchas veces acompañado de mi <strong>Saxofón con luces LED</strong>. Una fusión rompedora de <strong>DJ y Saxofón</strong> que creará el mejor ambiente de fiesta para no parar de bailar.</p>
+                            <h3 class="section-title" style="text-transform: uppercase;">Música disco & funk para fiestas </h3>
+                            <p class="section-subtext">Los grandes clasicos de la musica disco interpretados en saxofón, para que ponga a todo el mundo a bailar. La opción más acertada para arrancar una <strong>fiesta de boda en formato DJ y Saxo</strong>. Puedo inclir <strong>saxofón con luces LED</strong>, para una fiesta más dinámica y espectacular, tocando y animando desde la pista de baile con música en directo.</p>
                         </div>
                     </div>
                 </div>
 
                 <!-- COMMON -->
-                <?php include "common-php/videos/djsax.html"; ?>
+                <?php include "common-php/song-list/disco.html"; ?>
                 <!-- COMMON -->
 
-                <div class="row text-center">
-                    <a class="btn btn-small btn-black" href="#contact">Contacto</a>
-                </div>
             </div>
         </div>
 
@@ -114,7 +111,7 @@
                     <div class="row">
                         <div class="col-md-8 col-md-offset-2 col-xs-12">
                             <div class="text-center">
-	                            <h3 class="section-title">POP MODERNO</h3>
+	                            <h3 class="section-title" style="text-transform: uppercase;">Pop Moderno para el Cóctel</h3>
 	                            <p class="section-subtext">Selección de música moderna que interpreto en saxofón para <strong>ambientes tipo cocktail</strong>, como serían un <strong>aperitivo de boda</strong> o una moderna terraza en el un <strong>evento de empresa</strong>. Se trata de piezas donde juego la melodía principal en saxofón y que se acompañan perfectamente con un Gin tonic o Vermut musical.</p>
 
 	                           	<p>Recuerda que adapto el repertorio al gusto del cliente y estoy abierto a <strong>peticiones musicales</strong> que puedan tener, si no encuentran su pieza favorita en mi repertorio, avísenme, no hay nada que me motive más que aprender nuevas canciones.</p>
@@ -123,7 +120,7 @@
                     </div>
                     
                     <!-- COMMON -->
-                    <?php include "common-php/videos/wedding-cocktail.html"; ?>
+                    <?php include "common-php/song-list/modern-pop.html"; ?>
                     <!-- COMMON -->
 
                 </div>
@@ -135,20 +132,17 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 col-xs-12">
                         <div class="text-center">
-                            <h3 class="section-title">JAZZ & LOUNGE</h3>
-                            <p class="section-subtext">El estilo musical que marca mi carrera y estudios como saxofonista, también el estilo en el que más se encasilla el saxofón y del cual me <strong>apoyo para la improvisación como saxofonista junto a DJs</strong> en estilos House. El Jazz es un estilo perfecto par la <strong>ambientación</strong> de hoteles y espacios <strong>chill-out</strong>, con adaptaciones propias y modernas que me encanta combinar en un ambiente tipo Cocktail.</p>
+                            <h3 class="section-title" style="text-transform: uppercase;">Jazz & Chill</h3>
+                            <p class="section-subtext">El estilo musical que marca mi carrera y estudios como saxofonista, también el estilo en el que más se encasilla el saxofón y del cual me abor más a la improvisaciión como <strong>saxofonista y cantante</strong>. El Jazz es un estilo perfecto par la <strong>ambientación</strong> de hoteles y espacios <strong>chill-out</strong>, con adaptaciones propias y modernas que me encanta combinar en un ambiente tipo Cocktail.</p>
                         </div>
                     </div>
                 </div>
 
                 
                 <!-- COMMON -->
-                <?php include "common-php/videos/classics.html"; ?>
+                <?php include "common-php/song-list/singer.html"; ?>
                 <!-- COMMON -->
 
-                <div class="row text-center">
-                    <a class="btn btn-small btn-black" href="#contact">Contacto</a>
-                </div>
             </div>
         </div>
 
@@ -158,7 +152,7 @@
                     <div class="row">
                         <div class="col-md-8 col-md-offset-2 col-xs-12">
                             <div class="text-center">
-                                <h3 class="section-title">TROPICAL HOUSE & LO-FI HOUSE</h3>
+                                <h3 class="section-title" style="text-transform: uppercase;">Tropical House para un Cóctel o Fiesta</h3>
                                 <p class="section-subtext">Me gusta entender la música House como un espacio de <b>libertad y expresión</b>, donde la música más moderna pueda encontrarse con las raíces el Jazz y el Blues, especialmente acercándose en el caso del Lo-Fi House al Cool Blues que interpretava Miles Davies.</p>
 								<p>Este es un estilo música que mezclo junto a la <strong>música moderna</strong> y más radiofónica creando el perfecto ambiente Chill-Out que buscarías, por ejemplo en un <strong>evento tipo cocktail</strong> o la música para un <strong>aperitivo de boda</strong>. También trabajo con distintos DJs que pinchan música House y con los que creamos momentos únicos e irrepetibles, des de la improvisación y la sintonía.</p>
                             </div>
@@ -166,7 +160,7 @@
                     </div>
                     
                     <!-- COMMON -->
-                    <?php include "common-php/videos/latest.html"; ?>
+                    <?php include "common-php/song-list/tropical-house.html"; ?>
                     <!-- COMMON -->
                     
                 </div>

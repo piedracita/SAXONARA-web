ccd3306f0e5c82f31ae6b07c1eabd243f420632d
piedracita <arnaldpedros@gmail.com>
2024-04-09 15:45:28 +0200

    violin sax website

---
+++ violin-sax.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- COMMON-HEAD -->
+    <?php include "common-php/head.html"; ?>
+    <!-- COMMON-HEAD END -->
+
+    <!-- CANONICAL -->
+    <link rel="canonical" href="https://www.saxonara.com/violin-sax.php">
+
+    <!-- COMMON-LOCALIZATION -->
+    <?php include "common-php/localization/violin-sax.html"; ?>
+    <!-- COMMON-LOCALIZATION END -->
+
+    <!-- SITE INFORMATION -->
+    <title>Violín & Saxofón Fusión | Duo de Música | Eventos, Bodas y Fiestas</title>
+    <meta name="description" content="Fusión musical de Violín y Saxofón, el duo musical perfecto para eventos y bodas, disponible en Aperitivo, Cóctel, Fiesta y Electrónica House.">
+    <meta name="keywords" content="saxofonista, violinista, duo, fusion, bodas, fiestas, electrónica">
+
+    <!-- COMMON-OPENGRAPH -->
+    <?php include "common-php/opengraph/es.html"; ?>
+    <!-- COMMON-OPENGRAPH END -->
+    
+</head>
+
+<body>
+<!--<body class="page">-->
+
+    <!-- COMMON-BODY -->
+    <?php include "common-php/body.html"; ?>
+    <!-- COMMON-BODY END -->
+
+    <header id="top" class="top-header">
+        <!-- NAVIGATION -->
+        <?php include "common-php/navigation/es.html"; ?>
+        <!-- NAVIGATION END -->
+
+        <!-- BANNER SLIDER -->
+        <div class="banner-slider owl-slider" id="banner-slider">
+            <div class="banner-item banner-item-3">
+                <div class="banner-content text-white">
+                    <div class="container">
+                        <h1 class="banner-title">Violín & Saxofón Fusión</h1>
+                        <p class="banner-subtitle"><span class="banner-highlight">Fusión musical de Violín y Saxofón, el duo musical perfecto para eventos y bodas, disponible en Aperitivo, Cóctel, Fiesta y Electrónica House.</span></p>
+                        <div class="button-group">
+                            <a class="btn btn-border btn-lg btn-white" href="#contact">Contactar</a>
+                            <a class="btn btn-simple btn-lg btn-white" href="/videos/"><i class="fa-solid fa-play"></i> Ver Vídeos</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- BANNER SLIDER END -->
+    </header>
+
+    <div class="main-wrap">
+    	<!-- ABOUT BAND -->
+        <div class="section section-padding bottom-0 about-band-section" id="bio">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 col-xs-12">
+                        <div class="section-header text-center style-3">
+                            <h3 class="section-title">Dúo Violín & Saxofón</h3>
+                            <p class="section-subtext">Show expectacular con una puesta en escena vibrante, creando el mejor ambiente para tu evento. También estamos disponibles para <strong>bodas</strong>. Con nuestro <strong>Dúo Musical Violín & Saxo Fusión</strong> recorremos los temas más conocidos de la música española e internacional, con especial atención a esos temas que suenan hoy en día en las principales pistas de baile, sí como los clásicos de la música Remember y el Disco Funk de los 70s y 80s.</p>
+
+                            <p class="section-subtext">La mezcla de un <strong>violín electrónico</strong> con la energía de un saxofón en acústico, y también electrónico, serán el centro de atención en una fiesta, especialmente en eventos donde también puedan brillar las <strong>luces LED</strong> del violín y el <strong>saxo electrónico</strong>.</p>
+
+                            <p class="section-subtext">Como <strong>dúo musical</strong> también estamos disponibles para bodas, adaptando nuestro show <strong>Violín & Saxo Fusión</strong> a cada momento, desde <strong>Aperitivos tipo House / Ibiza</strong> a <strong>Fiestas con DJ</strong> y la música más actual. Así mismo también hay posibilidad de tocar en Ceremonia de boda y otras ideas.</p>
+
+                            <p class="section-subtext">Nos puedes contactar directamente para <strong>disponibilidad y precio</strong>. Estamos disponibles en toda España y el extrangero.</p>
+                            <a class="btn btn-small btn-black" href="#contact">CONTACTO</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 text-center">
+                        <div class="band-img">
+                            <img loading="lazy" class="img-responsive" src="images/violin-sax/violin-sax-fusion.jpg" alt="Fusión Violín Saxofón en Música Electrónica y bodas.">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ABOUT BAND END -->
+
+        <!-- DIVIDER -->
+        <?php include "common-php/violin-sax/divider-violin-sax-01-es.html"; ?>
+        <!-- DIVIDER END -->
+
+        <!-- ARTIST -->
+        <div class="section section-padding artist-section" id="videos">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 col-xs-12">
+                        <div class="section-header text-center">
+                            <h3 class="section-title">Fotos Violín y Sax Dúo</h3>
+                            <p class="section-subtext">Algunas de nuestras fotos favoritas en formato <strong>Dúo Violín y Saxofón</strong>, como nos encanta actuar en <strong>bodas</strong>, y como tenemos los mejores novios, siempre no esnvían fotos espectaculares. Nuestro formato y presentación se adapta al tipo de evento, no dudes en dejarnos tus comentarios cuando nos contrates.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <?php include "common-php/violin-sax/photos-violin-sax-01.html"; ?>
+
+            </div>
+        </div>
+        <!-- ARTIST END -->
+
+        <!-- WHAT WE DO -->
+        <div class="section black-bg side-img-section what-we-do-section" id="bands">
+            <div class="col-sm-6 col-sm-offset-6 side-img hidden-xs"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-5">
+                        <div class="doing-items section-padding">
+                            <div class="section-header">
+                                <h2 class="section-title">Preparamos el evento</h2>
+                                <p class="section-subtext">Conoce algunos detalles más de como preparamos cada evento, la coordinación entre nosotros en fundamental para una <b>perfecta interpretación</b> en el directo, así como la <b>coordinación con el cliente</b>. Cada evento o fiesta es distinta y nos gusta adaptarlo para que sea único.</p>
+                            </div>
+                            <div class="doing-item">
+                                <div class="doing-icon">
+                                    <i class="fa-solid fa-martini-glass-citrus"></i>
+                                </div>
+                                <h4 class="doing-title">Repertorio</h4>
+                                <p class="doing-text">A la hora de preparar el show es fundmental tener unas guías en cuanto a la música y el estilo que el cliente desa, no es lo mismo actuar en un <strong>Cóctel de boda</strong> que en una fiesta, así como tampoco es lo mismo interpretar temas de un <strong>evento corporativo</strong> en un auditorio que en un formato Lounge.</p>
+                            </div>
+                            <div class="doing-item">
+                                <div class="doing-icon">
+                                    <i class="fa-solid fa-church"></i>
+                                </div>
+                                <h4 class="doing-title">Presentación</h4>
+                                <p class="doing-text">Nuestros looks y el dress-code lo coordinaremos en el momento de la contratación, que puede ser el elegante habitual, <strong>fiesta Ibiza</strong> en blanco, formal de traje y/o de negro o en <strong>colaboración con marcas de ropa</strong> y similares.</p>
+                            </div>
+                            <div class="doing-item">
+                                <div class="doing-icon">
+                                    <i class="fa-solid fa-headphones"></i>
+                                </div>
+                                <h4 class="doing-title">Fiestas DJ</h4>
+                                <p class="doing-text">Nuestro formato de <strong>Dúo Violín & Saxofón Fusión</strong> es autónomo, nosotros mismos nos encargamos de mezclar la música y realizar nuestra actuación, sin embargo, hay posibilidad de trabajar con DJs externos, a quienes facilitaremos la música y con quienes podemos improvisar si fuera necesario, especialemente para un ambiente <strong>Jazz Chill-out</strong>.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- WHAT WE DO END -->
+
+        <!-- ABOUT ALBUM -->
+        <div class="section album-info-section section-padding" id="services">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="album-image">
+                            <img loading="lazy" class="img-responsive" src="images/violin-sax/violin-sax-duo.jpg" alt="Duo Violín y Saxofón para bodas,eventos y fiestas">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="section-header">
+                            <h2 class="section-title">Violín & Sax Fusión en Eventos</h2>
+                            <h5>Bodas</h5>
+                            <p>Una de las partes que más disfruto como saxofonista és cuando participo en bodas, realizando servicios como la <strong><a href="/saxofonista-bodas.php#ceremony">Ceremonia</a></strong>, el <strong><a href="/saxofonista-bodas.php#cocktail">Cóctel</a></strong>, el <strong>Primer Baile</strong> o la <strong><a href="/saxofonista-bodas.php#party">Fiesta junto al DJ</a></strong>, siempre con la intención de hacer que a celebración sea inolvidable y al gusto de la pareja.</p>
+                            <h5>Eventos Privados</h5>
+                            <p>En los <strong>eventos de empresa</strong> suelo trabajar <strong>amenizando en saxofón</strong> con música estilo Smooth Pop o House, creando un <strong>ambiente Chill-Out Moderno</strong> para momentos de After-Office, Team Building y similares, una opción muy solicitada en grandes ciudades como <strong>Barcelona, Madrid o Valencia</strong>.</p>
+                            <h5>Fiestas</h5>
+                            <p>También podemos proveer el <strong>equipo de sonido y luces</strong> para las performances, se trata de equipos de alta calidad y modulábles dependiendo del tipo de evento, el público y el espacio.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ABOUT ALBUM END -->
+
+    </div>
+    <!-- FOOTER -->
+    <?php include "common-php/footer/es.html"; ?>
+    <!-- FOOTER END -->
+
+    <!-- COMMON-BODY-SCRIPT -->
+    <?php include "common-php/body-script.html"; ?>
+    <!-- COMMON-BODY-SCRIPT END -->
+</body>
+
+</html>

5546059c1883850ad0d0f9db80448090e895e99d
piedracita <arnaldpedros@gmail.com>
2024-10-22 12:08:57 +0200

    Create onemanband.php

---
+++ onemanband.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- COMMON-HEAD -->
+    <?php include "common-php/head.html"; ?>
+    <!-- COMMON-HEAD END -->
+
+    <!-- CANONICAL -->
+    <link rel="canonical" href="https://www.saxonara.com/onemanband.php">
+
+    <!-- COMMON-LOCALIZATION -->
+<!-- REVISAR --> <?php include "common-php/localization/dj-sax.html"; ?>
+    <!-- COMMON-LOCALIZATION END -->
+
+    <!-- SITE INFORMATION -->
+    <title>One Man Band | Solista para evento cocktail o boda</title>
+    <meta name="description" content="La mejor opción musical para un aperitivo de boda con música en directo a guitarra, saxo y voz.">
+    <meta name="keywords" content="sax, guitarra, duo, saxo, bodas, coctel, eventos">
+
+    <!-- COMMON-OPENGRAPH -->
+    <?php include "common-php/opengraph/es.html"; ?>
+    <!-- COMMON-OPENGRAPH END -->
+    
+</head>
+
+<body>
+<!--<body class="page">-->
+
+    <!-- COMMON-BODY -->
+    <?php include "common-php/body.html"; ?>
+    <!-- COMMON-BODY END -->
+
+    <header id="top" class="top-header">
+        <!-- NAVIGATION -->
+        <?php include "common-php/navigation/es.html"; ?>
+        <!-- NAVIGATION END -->
+
+        <!-- BANNER SLIDER -->
+        <div class="banner-slider owl-slider" id="banner-slider">
+            <div class="banner-item banner-item-1">
+                <div class="banner-content text-white">
+                    <div class="container">
+                        <h1 class="banner-title">One Man Band</h1>
+                        <p class="banner-subtitle"><span class="banner-highlight">La mejor opción musical para un aperitivo de boda con música en directo a guitarra, saxo y voz.</span></p>
+                        <div class="button-group">
+                            <a class="btn btn-border btn-lg btn-white" href="#contact">Contactar</a>
+                            <!-- revisar --> <!-- <a class="btn btn-simple btn-lg btn-white" href="/videos/"><i class="fa-solid fa-play"></i> Ver Vídeos</a> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- BANNER SLIDER END -->
+    </header>
+
+    <div class="main-wrap">
+    	<!-- ABOUT BAND -->
+        <div class="section section-padding bottom-0 about-band-section" id="bio">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 col-xs-12">
+                        <div class="section-header text-center style-3">
+                            <h3 class="section-title">One Mand Band</h3>
+                            <p class="section-subtext">La mejor opción musical para un aperitivo de boda con música en directo a guitarra, saxo y voz.</p>
+
+                            <p class="section-subtext">El saxofón es un instrumentos que se mimetiza perfectamente con la música electrónica, desde estilos más <strong>Lounge</strong> y <strong>Chill-Out</strong> a las fiestas más explosivas, como podría ser un Club, con <strong>saxofón electrónico</strong>, o una Fiesta de Boda con el saxofonsita animando y ofreciendo <strong>música en directo</strong>.</p>
+
+                            <p class="section-subtext">A nuestro combo DJ & Sax se le pueden sumar más músicos para que el evento sea aún más vibrante, por ejemplo, <strong>DJ, Saxofón y Bongos</strong> es una opción fantástica para una fiesta de boda. Otra opción es <strong>DJ, Sax & Violín</strong> eléctrico, con una puesta en escena con <strong>luces LED</strong> y la música más conocida.</p>
+
+                            <p class="section-subtext">Nos puedes contactar directamente para <strong>disponibilidad y precio</strong>. Estamos disponibles en toda España y el extrangero.</p>
+                            <a class="btn btn-small btn-black" href="#contact">CONTACTO</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 text-center">
+                        <div class="band-img">
+                            <img loading="lazy" class="img-responsive" src="/images/dj-sax/dj-sax.jpg" alt="Combo DJ y Saxofonsita electrónico para fiestas y eventos.">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ABOUT BAND END -->
+
+        <!-- DIVIDER -->
+        <?php include "common-php/dj-sax/divider-dj-sax-01-es.html"; ?>
+        <!-- DIVIDER END -->
+
+        <!-- ARTIST -->
+        <div class="section section-padding artist-section" id="videos">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 col-xs-12">
+                        <div class="section-header text-center">
+                            <h3 class="section-title">DJ & Sax Electrónico</h3>
+                            <p class="section-subtext">Estas són algunas de nuestras fotos favoritas en fiestas DJ & SAX, también acompañados de otros músicos para formatos <strong>DJ, Sax & Bongos</strong> o a <strong>Dúo Saxo y Violín</strong>, encontrarás muchas más fotos en el Instagram de SAXONARA.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <?php include "common-php/dj-sax/photos-dj-sax-01-es.html"; ?>
+
+            </div>
+        </div>
+        <!-- ARTIST END -->
+
+        <!-- WHAT WE DO -->
+        <div class="section black-bg side-img-section what-we-do-section" id="bands">
+            <div class="col-sm-6 col-sm-offset-6 side-img hidden-xs"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-5">
+                        <div class="doing-items section-padding">
+                            <div class="section-header">
+                                <h2 class="section-title">Música en directo para eventos</h2>
+                                <p class="section-subtext">El formato perfecto para tu evento, la combinación musical <strong>DJ y Saxofón en directo</strong>, una opción potente, elegante y animada para hacer que tu evento destaque, disponible para <strong>eventos de empresa</strong>, <strong>bodas</strong> y otras opciones, sigue leyendo...</p>
+                            </div>
+                            <div class="doing-item">
+                                <div class="doing-icon">
+                                    <i class="fa-solid fa-martini-glass-citrus"></i>
+                                </div>
+                                <h4 class="doing-title">Chill-Out / Lounge</h4>
+                                <p class="doing-text">Si estás organizando un <strong>evento</strong> y quieres música en directo puedes contar con nuestro <strong>Combo DJ y Saxo</strong>, con nuestro formato obtendrás la <b>adaptabilidad</b> de la música DJ junto con el hype que le dará la <strong>música del saxofón en directo</strong>, puediendo adaptar la música a cada momento.</p>
+                            </div>
+                            <div class="doing-item">
+                                <div class="doing-icon">
+                                    <i class="fa-solid fa-water-ladder"></i>
+                                </div>
+                                <h4 class="doing-title">Pool Party</h4>
+                                <p class="doing-text">Mi opción favorita tanto en música como formato es la opción DJ & SAX para una pool party, mezclando <strong>música en directo</strong>, entretenimiento y el ambiente de <strong>Saxofonista Nikki Beach Ibiza</strong> en tu <strong>Pool Party</strong>.</p>
+                            </div>
+                            <div class="doing-item">
+                                <div class="doing-icon">
+                                    <i class="fa-solid fa-champagne-glasses"></i>
+                                </div>
+                                <h4 class="doing-title">La mejor fiesta</h4>
+                                <p class="doing-text">Un formató original de <strong>música en directo</strong> para <strong>fiestas de empresa</strong> y <strong>fiestas de boda</strong>, pudiéndonos adaptar en función de las necesidades y gustos de cliente, con una presentación <strong>elegante</strong> y saxofonista animando en formato pases desde la pista de baile, asegurando el éxito y <strong>diversión</strong> del evento.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- WHAT WE DO END -->
+
+        <!-- ABOUT ALBUM -->
+        <div class="section album-info-section section-padding" id="services">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="album-image">
+                            <img loading="lazy" class="img-responsive" src="images/dj-sax/dj-sax-combo.jpg" alt="Combo DJ y Saxo para fiestas y eventos como bodas">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="section-header">
+                            <h2 class="section-title">Saxofonista, Violín y Bongos</h2>
+                            <h5>Sax & Bongos</h5>
+                            <p>Junto con el DJ, este Combo <strong>Saxo & Bongos</strong> lenvatará el ánimo de la fiesta con el extra de <strong>percusión en directo</strong> y la energía del <strong>saxofón electrónico</strong>, un formato adaptable a bodas y eventos.</p>
+                            <h5>Violín & Saxofón</h5>
+                            <p>Preparamos los sets más explosivos para que no paréis de bailar, la combinación de <strong>Saxofón y Violín electrónico</strong> en directo hará que tu fiesta brille, especialmente con las <strong>luces LED</strong> del violín.</p>
+                            <h5>Eventos de empresa</h5>
+                            <p>Nuestros formatos de música en directo están disponibles para <b>eventos de empresa</b>, <strong>fiestas de boda</strong> y más eventos y fiestas privadas, así como conciertos y actuaciones. Consulta <strong>precio</strong> y disponibilidad.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ABOUT ALBUM END -->
+
+    </div>
+    <!-- FOOTER -->
+    <?php include "common-php/footer/es.html"; ?>
+    <!-- FOOTER END -->
+
+    <!-- COMMON-BODY-SCRIPT -->
+    <?php include "common-php/body-script.html"; ?>
+    <!-- COMMON-BODY-SCRIPT END -->
+</body>
+
+</html>

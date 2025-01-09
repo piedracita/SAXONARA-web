22d04feaf4ed84b226b69708b199e6655aabfc73
piedracita <arnaldpedros@gmail.com>
2025-01-09 17:40:53 +0100

    spanish guitar anglès

---
+++ en/spanish-guitar.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- COMMON-HEAD -->
+    <?php include "../common-php/head.html"; ?>
+    <!-- COMMON-HEAD END -->
+
+    <!-- CANONICAL -->
+    <link rel="canonical" href="https://www.saxonara.com/en/spanish-guitar.php">
+
+    <!-- COMMON-LOCALIZATION -->
+    <?php include "../common-php/localization/spanish-guitar.html"; ?>
+    <!-- COMMON-LOCALIZATION END -->
+
+    <!-- SITE INFORMATION -->
+    <title>Spanish Guitar on weddings and events | Available in Barcelona, Sitges, Costa Brava and Spain</title>
+    <meta name="description" content="Spanish guitarist for weddings and events, instrumental music on spanish gutiar and optionally saxophone too! Hire me on spanish guitar in Barcelona, Sitges, Costa Brava and Spain.">
+    <meta name="keywords" content="spanish, guitar, sax, events, weddings, hire">
+
+    <!-- COMMON-OPENGRAPH -->
+    <?php include "../common-php/opengraph/en.html"; ?>
+    <!-- COMMON-OPENGRAPH END -->
+    
+</head>
+
+<body>
+<!--<body class="page">-->
+
+    <!-- COMMON-BODY -->
+    <?php include "../common-php/body.html"; ?>
+    <!-- COMMON-BODY END -->
+
+    <header id="top" class="top-header">
+        <!-- NAVIGATION -->
+        <?php include "../common-php/navigation/en.html"; ?>
+        <!-- NAVIGATION END -->
+
+        <!-- BANNER SLIDER -->
+        <div class="banner-slider owl-slider" id="banner-slider">
+            <div class="banner-item banner-item-spanish-guitar">
+                <div class="banner-content text-white">
+                    <div class="container">
+                        <h1 class="banner-title">Spanish Guitar for weddings and events</h1>
+                        <p class="banner-subtitle"><span class="banner-highlight">Spanish guitarist for weddings and events, instrumental music on spanish gutiar and optionally saxophone too! Hire me on spanish guitar in Barcelona, Sitges, Costa Brava and Spain.</span></p>
+                        <div class="button-group">
+                            <a class="btn btn-border btn-lg btn-white" href="#contact">Contact</a>
+                            <!-- <a class="btn btn-simple btn-lg btn-white" href="/en/videos.php"><i class="fa-solid fa-play"></i> Videos</a>-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- BANNER SLIDER END -->
+    </header>
+
+    <div class="main-wrap">
+    	<!-- ABOUT BAND -->
+        <div class="section section-padding bottom-0 about-band-section" id="bio">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 col-xs-12">
+                        <div class="section-header text-center style-3">
+                            <h3 class="section-title">Spanish Guitar for weddings and events</h3>
+                            <p class="section-subtext">Would you like to create a cocktail atmosphere for your <strong>event</strong> or <strong>wedding</strong> with music on <strong>Spanish guitar</strong>? Perhaps you've arrived here looking for a saxophonist for your event but are thinking about also having <strong>spanish guitarist</strong>, in which case you're in luck for both ideas.</p>
+
+                            <p class="section-subtext">My name Arnald SAXONARA, <strong>saxophonist and guitarist</strong>, and in this section I'll tell you about how I like to include the Spanish guitar in the events I participate in.</p>
+
+                            <p class="section-subtext">If you want to check availability and receive a quote, contact me directly telling me about your interest and the date of the event, you will receive an approximate quote immediately.</p>
+
+                            <a class="btn btn-small btn-black" href="#contact">CONTACT</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 text-center">
+                        <div class="band-img">
+                            <img loading="lazy" class="img-responsive" src="/images/spanish-guitar/spanish-guitar.jpg" alt="DJ and electronic saxophonist band at parties and events.">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ABOUT BAND END -->
+
+        <!-- DIVIDER -->
+        <?php include "../common-php/spanish-guitar/divider-01-en.html"; ?>
+        <!-- DIVIDER END -->
+
+        <!-- ARTIST -->
+        <div class="section section-padding artist-section" id="videos">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 col-xs-12">
+                        <div class="section-header text-center">
+                            <h3 class="section-title">Cocktail Events on spanish guitar</h3>
+                            <p class="section-subtext">Spanish guitar can play many music styles, and can be played in different adaptations and have a very wide repertoire with just the guitar and its <strong>flamenco</strong> strumming. However, the music can also be taken to more <strong>Bossa Nova</strong> and traditional styles.</p>
+
+                            <p class="section-subtext">A <strong>cocktail atmosphere with Spanish guitar music</strong> usually includes basic sound (a set of one or two speakers) that allows the music to spread throughout the space.</p>
+
+                            <p class="section-subtext">The repertoire will be prepared together with the client, who can give ideas on where to take the musical style.</p>
+
+                            <p class="section-subtext"><strong>Solo Spanish guitarist</strong> performances are usually instrumental, but vocals can be included at the client's request.</p>
+
+                            <p class="section-subtext">The option of including saxophone in the Spanish guitar show is always highly recommended as it gives a much brighter and more surprising touch to the performance, a unique characteristic of my show and versatility.</p>
+
+                        </div>
+                    </div>
+                </div>
+
+                <?php include "../common-php/spanish-guitar/photos-spanish-guitar-01-en.html"; ?>
+
+            </div>
+        </div>
+        <!-- ARTIST END -->
+
+        <!-- WHAT WE DO -->
+        <div class="section black-bg side-img-section what-we-do-section" id="bands">
+            <div class="col-sm-6 col-sm-offset-6 side-img hidden-xs"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-5">
+                        <div class="doing-items section-padding">
+                            <div class="section-header">
+                                <h2 class="section-title">Spanish Guitar at Weddings</h2>
+                                <p class="section-subtext">Can you think of a better atmosphere for your wedding in Spain than adding a spanish guitar to it?</p>
+                            </div>
+                            <div class="doing-item">
+                                <div class="doing-icon">
+                                    <i class="fa-solid fa-martini-glass-citrus"></i>
+                                </div>
+                                <h4 class="doing-title">Welcoming Guests</h4>
+                                <p class="doing-text">Offer your guests an warm welcome with live <strong>Spanish music</strong> in a relaxed atmosphere as they arrive at the wedding venue or ceremony area, this also applies to the <strong>Pre-ceremony</strong>, while guets come in and wait.</p>
+                            </div>
+                            <div class="doing-item">
+                                <div class="doing-icon">
+                                    <i class="fa-solid fa-water-ladder"></i>
+                                </div>
+                                <h4 class="doing-title">Ceremony</h4>
+                                <p class="doing-text">Choose your favourite music for the ceremony and I will play it on a Spanish guitar, accompanied by a musical base, for better sound and quality. Optionally the ceremony can also be played on <strong>saxophone</strong>, remember that I double on both instruments!</p>
+                            </div>
+                            <div class="doing-item">
+                                <div class="doing-icon">
+                                    <i class="fa-solid fa-champagne-glasses"></i>
+                                </div>
+                                <h4 class="doing-title">Cocktail Vibes</h4>
+                                <p class="doing-text">The <strong>perfect wedding aperitif</strong>, a chill atmosphere with <strong>Spanish guitar music</strong> and saxophone melodies, the musical combination that will surprise your guests, undoubtedly a unique option in Spain, available in <strong>Barcelona</strong>, <strong>​​Costa Brava</strong> and <strong>Sitges</strong>, but I also travel throughout Spain, France and Italy.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- WHAT WE DO END -->
+
+        <!-- ABOUT ALBUM -->
+        <div class="section album-info-section section-padding" id="services">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="album-image">
+                            <img loading="lazy" class="img-responsive" src="/images/spanish-guitar/guitar-sax-combo.jpg" alt="Spanish guitar and saxophone for the best chill out music event">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="section-header">
+                            <h2 class="section-title">Guitar & Saxophone</h2>
+                            <h5>Unique Show</h5>
+                            <p>A unique fusion, a <strong>guitar and saxophone</strong> show with live music creation, contributing all my talent and the knowledge of a life dedicated to music, the <strong>one-man-band</strong> concept in its purest form that does not need pre-recorded music and everything is born at the same time, so each performance is unique.</p>
+                            <h5>Perfect for Chill-out events</h5>
+                            <p>Apart from my concerts, the event format that best fits the guitar & saxophone format are cocktail-type events, where the aim is to create a <strong>chill-out music</strong> atmosphere with ambient music that allows guests to talk, offering a <strong>high quality musica show</strong>.</p>
+                            <h5>Live performances</h5>
+                            <p>Also available for open events like terraces, hotels, bars by the sea in <strong>Ibiza</strong>... Get in touch for more information!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ABOUT ALBUM END -->
+
+    </div>
+    <!-- FOOTER -->
+    <?php include "../common-php/footer/en.html"; ?>
+    <!-- FOOTER END -->
+
+    <!-- COMMON-BODY-SCRIPT -->
+    <?php include "../common-php/body-script.html"; ?>
+    <!-- COMMON-BODY-SCRIPT END -->
+</body>
+
+</html>

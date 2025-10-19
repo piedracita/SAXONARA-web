4ec923f0e82b00720ec325c39c3784baffac22f5
piedracita <arnaldpedros@gmail.com>
2025-10-19 23:32:26 +0200

    djsaxcat

---
+++ cat/dj-sax.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="ca">
+
+<head>
+    <!-- COMMON-HEAD -->
+    <?php include "../common-php/head.html"; ?>
+    <!-- COMMON-HEAD END -->
+
+    <!-- CANONICAL -->
+    <link rel="canonical" href="https://www.saxonara.com/cat/dj-sax.php">
+
+    <!-- COMMON-LOCALIZATION -->
+    <?php include "../common-php/localization/dj-sax.html"; ?>
+    <!-- COMMON-LOCALIZATION END -->
+
+    <!-- SITE INFORMATION -->
+    <title>DJ & SAXO Combo | Esdeveniments i Festes de casament | + Bongos / Violí</title>
+    <meta name="description" content="Música en directe DJ & SAXO Combo per a Festes i Esdeveniments, també amb Bongos o Violí. Música en directe tipus Lounge/Chill-Out o les Festes més explosives en bodes i clubs de Barcelona i Catalunya. Consulta preu i disponibilitat.">
+    <meta name="keywords" content="saxo, dj, duo, combo, bodes, festes, electrònica, música en directe, lounge, chill-out, bongos, violí, esdeveniments, barcelona, catalunya">
+
+    <!-- COMMON-OPENGRAPH -->
+    <?php include "../common-php/opengraph/cat.html"; ?>
+    <!-- COMMON-OPENGRAPH END -->
+    
+</head>
+
+<body>
+<!--<body class="page">-->
+
+    <!-- COMMON-BODY -->
+    <?php include "../common-php/body.html"; ?>
+    <!-- COMMON-BODY END -->
+
+    <header id="top" class="top-header">
+        <!-- NAVIGATION -->
+        <?php include "../common-php/navigation/cat.html"; ?>
+        <!-- NAVIGATION END -->
+
+        <!-- COMMON-BANNER-DJSAX -->
+        <?php include "../common-php/banner/djsax/es.html"; ?>
+        <!-- /COMMON-BANNER-DJSAX -->
+    </header>
+
+    <div class="main-wrap">
+        <!-- COMMON-DJSAX-HEADER -->
+        <?php include "../common-php/dj-sax/header-cat.html"; ?>
+        <!-- /COMMON-DJSAX-HEADER -->
+
+        <!-- COMMON-DJSAX-DIVIDER -->
+        <?php include "../common-php/dj-sax/divider-dj-sax-01-cat.html"; ?>
+        <!-- /COMMON-DJSAX-DIVIDER -->
+
+        <!-- COMMON-DJSAX-IBIZASUNSET -->
+        <?php include "../common-php/dj-sax/ibizasunset-cat.html"; ?>
+        <!-- /COMMON-DJSAX-IBIZASUNSET -->
+
+        <!-- COMMON-DJSAX-live-music-events -->
+        <?php include "../common-php/dividers/live-music-events-cat.html"; ?>
+        <!-- /COMMON-DJSAX-live-music-events -->
+        
+        <!-- COMMON-DJSAX-ELECTRONICSAX -->
+        <?php include "../common-php/dj-sax/electronicdjsax-cat.html"; ?>
+        <!-- COMMON-DJSAX-ELECTRONICSAX -->
+
+        <!-- DJ SAX VIDEOS -->
+        <div class="section purchase-section text-white">
+            <div class="overlay section-padding">
+                <div class="container">
+                <!-- VIDEOS DJSAX -->
+                <?php include "../common-php/videos/djsax.html"; ?>
+                <!-- /VIDEOS DJSAX -->
+                </div>
+            </div>
+        </div>
+        <!-- /DJ SAX VIDEOS -->
+
+        <!-- COMMON-DJSAX-DJ-SAX-VIOLIN-PERCUSSION -->
+        <?php include "../common-php/dj-sax/dj-sax-violin-percussion-cat.html"; ?>
+        <!-- COMMON-DJSAX-DJ-SAX-VIOLIN-PERCUSSION -->
+
+    </div>
+    <!-- FOOTER -->
+    <?php include "../common-php/footer/cat.html"; ?>
+    <!-- FOOTER END -->
+
+    <!-- COMMON-BODY-SCRIPT -->
+    <?php include "../common-php/body-script.html"; ?>
+    <!-- COMMON-BODY-SCRIPT END -->
+</body>
+
+</html>

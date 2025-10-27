6a71e2c22381d1020be7adeec31943eb9f3d786a
piedracita <arnaldpedros@gmail.com>
2025-10-27 12:53:17 +0100

    update saxo barcelona

--- saxofonista-barcelona.php
+++ saxofonista-barcelona.php
@@ -61,13 +61,12 @@
                 <div class="row">
                     <div class="col-md-6 col-md-push-6 col-xs-12 col-xs-push-0">
                         <div class="about-artist">
-                            <h2 class="artist-name">Arnald | SAXONARA</h2>
+                            <h2 class="artist-name">Arnald SAXONARA</h2>
                             <p><b>Saxofonista profesional de Barcelona con larga experiencia sobre los escenarios, también como músico para bodas, eventos y fiestas.</b></p>
                             <p>¡Hola! Soy Arnald SAXONARA, saxofonista profesional con años de experiencia sobre los escenarios, tocando distintos estilos y cursando en academias de Barcelona, Estados Unidos, Puerto Rico y Argentina. Se me conoce con el nombre artístico SAXONARA como músico <strong>saxofonista para bodas y eventos</strong> donde mezclo la música al saxofón con espectáculos y shows divertidos, buscando la interacción con los invitados.</p>
                             <p>Barcelona es mi ciudad favorita para tomar parte de eventos exclusivos y de lujo, tiene una <b>gran vida cultural</b> y muchos centros de negocios que hacen que sea muy interesante para que se realicen <b>actividades locales e internacionales</b>. Además, también cuenta con un entorno privilegiado para la realización de <strong>bodas</strong>, con fincas, viñedos y hoteles de lujo.</p>
                             <div class="artist-information-list">
                                 <p><b class="artist-info-item">Nombre</b>:<span class="artist-info-value">Arnald SAXONARA</span></p>
-                                <p><b class="artist-info-item">Edad</b>:<span class="artist-info-value">32 años</span></p>
                                 <p><b class="artist-info-item">Saxofonista en</b>:<span class="artist-info-value">Barcelona</span></p>
                                 <p><b class="artist-info-item">Educación</b>:<span class="artist-info-value">Academias de España, USA y Puerto Rico</span></p>
                                 <p><b class="artist-info-item">Sígueme</b>:<span class="artist-info-value">
@@ -127,34 +126,17 @@
         </div>
         <!-- WHAT WE DO END -->
 
-        <!-- ARTIST -->
-        <div class="section section-padding artist-section" id="videos">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2 col-xs-12">
-                        <div class="section-header text-center">
-                            <h3 class="section-title">Grabaciones en Barcelona</h3>
-                            <p class="section-subtext">Descubre algunos de mis videoclips grabados en Barcelona, donde te muestro como son las experiencias de realizar eventos en la mejor ciudad de España, <strong>saxofonista en bodas y fiestas</strong> en rincones únicos que no todos los barceloneses conocen de su existencia. Para la grabación de estos vídeos utilizo distintos saxofones, principalmente el saxofón tenor y el saxofón alto, pero también el soprano para momentos más íntimos como sería una <strong>Ceremonia de boda</strong>.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <?php include "common-php/videos/loc-barcelona.html"; ?>
-
-                <div class="row">
-                        <div class="col-md-10 col-md-offset-1 col-xs-12">
-                            <div class="text-center style-3">
-                                <a class="btn btn-small btn-black" href="/videos/" title="Videos de SAXONARA">MÁS VÍDEOS</a>
-                            </div>
-                        </div>
-                    </div>
-            </div>
-        </div>
-        <!-- ARTIST END -->
+        <!-- COMMON-PHP RECORDINGS-BARCELONA -->
+        <?php include "common-php/pages-content/rec-barcelona-es.html"; ?>
+        <!-- /COMMON-PHP RECORDINGS-BARCELONA -->
 
         <!-- DIVIDER -->
         <?php include "common-php/dividers/01-es.html"; ?>
-        <!-- DIVIDER END -->
+        <!-- /DIVIDER -->
+
+        <!-- COMMON-PHP RECORDINGS-BARCELONA -->
+        <?php include "common-php/videos/loc-barcelona.html"; ?>
+        <!-- /COMMON-PHP RECORDINGS-BARCELONA -->
 
     </div>
     <!-- FOOTER -->

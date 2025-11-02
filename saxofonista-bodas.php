91e521e9026f37ec31f5a909acecd9de4dc6133a
piedracita <arnaldpedros@gmail.com>
2025-11-02 22:39:05 +0100

    uupdate bodas

--- saxofonista-bodas.php
+++ saxofonista-bodas.php
@@ -91,44 +91,9 @@
         <?php include "common-php/pages-content/weddings/wedding-party-es.html"; ?>
         <!-- /COMMON-PAGE WEDDING-PARTY -->
 
-        <!-- WHAT WE DO -->
-        <div class="section black-bg side-img-section what-we-do-section" id="bands">
-            <div class="col-sm-6 col-sm-offset-6 side-img hidden-xs"></div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-5">
-                        <div class="doing-items section-padding">
-                            <div class="section-header">
-                                <h2 class="section-title" id="more">Más Ideas</h2>
-                                <p class="section-subtext">Cada boda es única y yo trato de adaptarme a vuestro gusto, por eso la descripción de cada servicio es orientativa, así pues, trabajaré cada propuesta para <b>adaptar la música a vuestras peticiones</b> y en coordinación con el equipo de la finca, del DJ, de los Wedding Planners o de parientes y amigos que colaboran para que vuestro día sea único. A continuación os detallo otras ideas, distintas a los servicios principales, que algunos novios me han solicitado a lo largo de mi carrera:</p>
-                            </div>
-                            <div class="doing-item">
-                                <div class="doing-icon">
-                                    <i class="fa-solid fa-star"></i>
-                                </div>
-                                <h4 class="doing-title">Apariciones espectaculares</h4>
-                                <p class="doing-text">Recuerdo en una ocasión donde aparecí al final del banquete, por sorpresa, <b>desde el balcón de una finca</b>, con todos los invitados que estaban terminando de comer abajo, fue espectacular. Interpreté tres temas, uno desde el balcón, otro bajando con los invitados y el tercero llevándome a todo el mundo, haciendo la conga, hasta la zona de la fiesta. Como no, lo unimos al servicio de <b>Fiesta</b> tocando junto al DJ.</p>
-                            </div>
-                            <div class="doing-item">
-                                <div class="doing-icon">
-                                    <i class="fa-solid fa-utensils"></i>
-                                </div>
-                                <h4 class="doing-title">Ambientación del banquete</h4>
-                                <p class="doing-text">En este momento tenemos dos opciones, de un lado crear una <strong>ambientación suave</strong>, de fondo, con música al saxofón acompañado de guiarra o bases;  o bien realizar 2 o 3 pases durante el banquete, con <b>música explosiva</b> para crear ambiente de fiesta, incluso coordinándolo con los regalos o la entrada de los novios.</p>
-                            </div>
-                            <div class="doing-item">
-                                <div class="doing-icon">
-                                    <i class="fa-solid fa-music"></i>
-                                </div>
-                                <h4 class="doing-title">El primer baile</h4>
-                                <p class="doing-text">Esta opción suele ir ligada al servicio de la Fiesta junto al DJ, para el primer baile prepararía la <b>música a petición de la pareja</b>, creando las pistas que durarían un tiempo determinado, así los recién casados podrían preparar el baile.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- /WHAT WE DO -->
+        <!-- COMMON-PAGE WEDDING-IDEAS -->
+        <?php include "common-php/pages-content/weddings/wedding-ideas-es.html"; ?>
+        <!-- /COMMON-PAGE WEDDING-IDEAS -->
 
         <!-- REVIEWS -->
         <?php include "common-php/reviews/es.html"; ?>
